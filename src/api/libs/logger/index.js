6adf396783291e6d65f02d3f19ec67848e7fa8f3
Tarsis de Lima <br.tmvdl@gmail.com>
2023-02-11 17:46:34 -0300

    Fixing LOGGER library files

--- src/api/libs/logger/index.js
+++ src/api/libs/logger/index.js
@@ -1,8 +1,7 @@
 const { ENV } = require('../../config')
 
-const logger = (name, log = {}, writter = console.log) => {
-  if (ENV == 'development') writter(name, (log))
-}
+const logger = (name, log = {}, writter = console.log) => 
+  (ENV == 'development') ? writter(name, (log)) : (() => {})()
 
 module.exports = {
   error: (name, log = {}) => logger(name, log, console.error),
