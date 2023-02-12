14c74efbdd78ab418919c7ece6c0a2d561ad06af
Tarsis de Lima <br.tmvdl@gmail.com>
2023-02-12 14:54:58 -0300

    Fixing FILESYSTEM library files

--- src/api/libs/fs/index.js
+++ src/api/libs/fs/index.js
@@ -1,16 +1,15 @@
 const fs = require('fs')
 const path = require('path')
 
+const isFile = (name) => fs.statSync(name).isFile()
+
 const mkdirSync = (name) => fs.mkdirSync(path.resolve(name), { recursive: true })
 
 const writeFileSync = (name, value = '') => fs.writeFileSync(name, value)
 
-const readFileSync = (name) => fs.readFileSync(name)
+const readFileSync = (name) => isFile(name) ? fs.readFileSync(name) : ''
 
-const readdirSync = (name) => {
-  mkdirSync(name)
-  fs.readdirSync(path.resolve(name))
-}
+const readdirSync = (name) => [mkdirSync(name), fs.readdirSync(name)][1]
 
 module.exports = {
   mkdirSync,
