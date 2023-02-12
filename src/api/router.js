b48ea9c5dc430773e0333066e2c13769016c3f45
Tarsis de Lima <br.tmvdl@gmail.com>
2023-02-12 14:55:51 -0300

    Fixing ROUTER logging

--- src/api/router.js
+++ src/api/router.js
@@ -4,10 +4,14 @@ class Doer {
   func = null
 
   do(f = (() => { })) {
+    info('router/Doer.do', { f })
+
     this.func = f
   }
 
   doing(req, res) {
+    info('router/Doer.doing', { req, res })
+
     return this.func(req, res)
   }
 }
@@ -16,7 +20,7 @@ class Router {
   routes = []
 
   use(name = '') {
-    info('Router.use', { name })
+    info('router/Router.use', { name })
 
     const doer = new Doer(name)
     this.routes.push({ name, doer })
